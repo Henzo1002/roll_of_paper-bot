d64671b8c02f9aa3aea4918cf6593e8a3031bb94
Henzo1002 <galinannechi@gmail.com>
2021-08-19 00:16:08 +0200

    amk

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const prefix = 'r';
 
 
 
+//haupt ding
 client.on("messageCreate", msg => msg.react('🧻'));
 
 
@@ -43,6 +44,53 @@ client.on('message', message => {
 		message.reply('<:sbetflushed:793643748851843102> best website on earth, if you disagree ill shoot your fucking cat<:sbetgun:799132599922589776>. https://www.swagmoneygaming.com <:sbetsexy:792311017107488769> (my favorite)');
 	}
 });
+
+
+client.on('message', message => {
+	if (message.content === 'peepee') {
+		message.reply('I love peepee osu, if you disagree on my opinion ill fucking stab your dumb dog <:YanK1:421040453912363009><:YanK2:421040453782208527><:YanK3:421040454147244032>🐶<:YanK4:421040453845123084>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === 'i forgor') {
+		message.reply('<:iforgor:869465394262441994>');
+		message.react('<:iforgor:869465394262441994>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === 'i rember') {
+		message.reply('<:irember:869465367322427493>');
+		message.react('<:irember:869465367322427493>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === '<:irember:869465367322427493>') {
+		message.react('<:irember:869465367322427493>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === '<:iforgor:869465394262441994>') {
+		message.react('<:iforgor:869465394262441994>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === 'i hate roll_of_paper bot') {
+		message.reply('<:sbetdepressed:798439572484390923>');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === 'Henzo') {
+		message.stickers('');
+	}
+});
+
+
 
 
 
